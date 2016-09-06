8684b691662b161d17d26e7dade884867df3c297
lucono <lucono.mail@gmail.com>
2016-09-06 02:06:05 -0400

    Updates and fixes to code playground

--- screens/play/playScreen.js
+++ screens/play/playScreen.js
@@ -1,99 +1,109 @@
 /* global prettyPrint */
 /// <reference path="../../typings/angularjs/angular.d.ts"/>
 
-'use strict';
+(function() {
 
-angular.module('xtypejsSite')
+    'use strict';
 
-    .controller('PlayScreenController', ['$rootScope', '$scope', 'service', function($rootScope, $scope, service) {
+    var defaultCodeSnippet = "var xtype = require('xtypejs'); \n\n\n";
 
-        $scope.defaultCodeSnippet = "var xtype = require('xtypejs');";
-        $scope.defaultCodeSnippetTitle = 'Code Playground';
-        $scope.codeSnippetTitle = $scope.defaultCodeSnippetTitle;
-        
-        service.getCodeContent('play', function(playContent) {
+
+    function loadCodeSnippet(codeEditor, codeSnippetItem) {
+
+        if (!codeSnippetItem) {
+            codeEditor.setSource(defaultCodeSnippet);
+            return;
+        }
+
+        var codeSnippet = codeSnippetItem.content,
+            codeSnippetCategory = codeSnippetItem.attributes.category,
+            codeSnippetTitle = codeSnippetItem.attributes.title;
             
+        codeSnippet = 
+            '// ' + codeSnippetCategory + ': ' + codeSnippetTitle + '\n\n' +
+            codeSnippet.trim() + '\n\n\n' +
+            '// Play with this code\n\n';
+
+        codeEditor.setSource(codeSnippet);
+    }
+
+
+    angular.module('xtypejsSite')
+
+        .controller('PlayScreenController', ['$rootScope', '$scope', 'service', function($rootScope, $scope, service) {
+
             $scope.codeEditor = Tonic.createNotebook({
                 element: document.getElementById('play_editor_box'),
-                source: $scope.defaultCodeSnippet
+                source: defaultCodeSnippet
+                //onLoad: loadCodeSnippet
             });
 
-            $scope.playContent = playContent;
-            
-            var contentNames = $rootScope.AppUtils.keys(playContent),
-                itemsByCategory = {},
-                itemsByCategoryAndColGroups = {};
-
-            contentNames.forEach(function(contentName) {
-                var contentItem = playContent[contentName],
-                    itemCategory = contentItem.attributes.category,
-                    categoryItems = itemsByCategory[itemCategory];
-
-                if (!categoryItems) {
-                    categoryItems = [];
-                    itemsByCategory[itemCategory] = categoryItems;
-                }
-
-                categoryItems.push(contentItem);
-            });
-
-            $rootScope.AppUtils.keys(itemsByCategory).forEach(function(category) {
-                var categoryItems = itemsByCategory[category],
-                    col1Items = [],
-                    col2Items = [],
-                    midIndex = Math.ceil(categoryItems.length / 2),
-                    contentIndex
+            service.getCodeContent('play', function(playContent) {
                 
-                for (contentIndex = 0; contentIndex < categoryItems.length; contentIndex++) {
-                    (contentIndex < midIndex ? col1Items : col2Items).push(categoryItems[contentIndex]);
-                }
+                $scope.playContent = playContent;
+                
+                var contentNames = $rootScope.AppUtils.keys(playContent),
+                    itemsByCategory = {},
+                    itemsByCategoryAndColGroups = {};
 
-                itemsByCategoryAndColGroups[category] = {
-                        col1Items: col1Items,
-                        col2Items: col2Items
-                    };
+                contentNames.forEach(function(contentName) {
+                    var contentItem = playContent[contentName],
+                        itemCategory = contentItem.attributes.category,
+                        categoryItems = itemsByCategory[itemCategory];
+
+                    if (!categoryItems) {
+                        categoryItems = [];
+                        itemsByCategory[itemCategory] = categoryItems;
+                    }
+
+                    categoryItems.push(contentItem);
+                });
+
+                $rootScope.AppUtils.keys(itemsByCategory).forEach(function(category) {
+                    var categoryItems = itemsByCategory[category],
+                        col1Items = [],
+                        col2Items = [],
+                        midIndex = Math.ceil(categoryItems.length / 2),
+                        contentIndex
+                    
+                    for (contentIndex = 0; contentIndex < categoryItems.length; contentIndex++) {
+                        (contentIndex < midIndex ? col1Items : col2Items).push(categoryItems[contentIndex]);
+                    }
+
+                    itemsByCategoryAndColGroups[category] = {
+                            col1Items: col1Items,
+                            col2Items: col2Items
+                        };
+                });
+                
+                $scope.contentNames = contentNames;
+                $scope.itemsByCategoryAndColGroups = itemsByCategoryAndColGroups;
             });
             
-            $scope.contentNames = contentNames;
-            //$scope.col1Items = col1Items;
-            //$scope.col2Items = col2Items;
-
-            $scope.itemsByCategoryAndColGroups = itemsByCategoryAndColGroups;
-        });
+            $rootScope.activeScreen = 'play';
+            $rootScope.screenTitle = 'Play';
+        }])
         
-        $rootScope.activeScreen = 'play';
-        $rootScope.screenTitle = 'Play';
-    }])
-    
-    .controller(
-            'loadPlayCodeSnippet', ['$rootScope', '$scope', '$state', '$stateParams', '$timeout', 
-            function($rootScope, $scope, $state, $stateParams, $timeout) {
-
-        var codeEditor = $scope.codeEditor,
-            codeSnippetId = $stateParams.item,
-            codeSnippetItem = $scope.playContent[codeSnippetId],
-            codeSnippet = (codeSnippetItem && codeSnippetItem.content ? codeSnippetItem.content : $scope.defaultCodeSnippet),
-            codeSnippetCategory = (codeSnippetItem ? codeSnippetItem.attributes.category + ': ' : ''),
-            codeSnippetTitle = (codeSnippetItem ? codeSnippetItem.attributes.title : $scope.defaultCodeSnippetTitle);
+        .controller(
+                'loadPlayCodeSnippet', ['$rootScope', '$scope', 'service', '$state', '$stateParams', '$timeout', 
+                function($rootScope, $scope, service, $state, $stateParams, $timeout) {
             
-            codeSnippet = 
-                '// ' + codeSnippetCategory + codeSnippetTitle + '\n\n' +
-                codeSnippet.trim() + '\n\n\n' +
-                '// Play with this code\n\n';
+            var playEditorId = 'play_editor',
+                isSameScreenNavigation = ($rootScope.previousState && $state.includes($rootScope.previousState.name.split('\.')[0]));
 
-        var playEditorId = 'play_editor',
-            isSameScreenNavigation = ($rootScope.previousState && $state.includes($rootScope.previousState.name.split('\.')[0]));
-        
-        function scrollToEditorAndLoadCodeSnippet() {
+            if ($stateParams.item === 'menu') {
+                $rootScope.navigateToItem($stateParams.item, !isSameScreenNavigation);
+                return;
+            }
+
+            if ($scope.playContent) {
+                loadCodeSnippet($scope.codeEditor, $scope.playContent[$stateParams.item]);
+            } else {
+                service.getCodeContent('play', function(playContent) {
+                    loadCodeSnippet($scope.codeEditor, playContent[$stateParams.item]);
+                });
+            }
+
             $rootScope.navigateToItem(playEditorId, !isSameScreenNavigation);
-            codeEditor.setSource(codeSnippet);
-        }
-
-        if (isSameScreenNavigation) {
-            scrollToEditorAndLoadCodeSnippet();
-        } else {
-            $timeout(function() {
-                scrollToEditorAndLoadCodeSnippet();
-            });
-        }
-    }]);
+        }]);
+})();
